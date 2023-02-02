bb8ee3cdd9cd80d248f26525b6e64b29105ca12b
AngeloCalabro <angelo.1991@hotmail.com>
2023-02-02 11:51:04 +0100

    check-footer

--- src/store.js
+++ src/store.js
@@ -4,5 +4,47 @@ export const store = reactive({
 
     apiUrl: 'http://127.0.0.1:8000/api',
     imagePath: 'http://127.0.0.1:8000/storage/',
-    
+
+    mainMenuItem: [
+        'Contact US',
+        'Help Center / FAQs',
+        'Shipping',
+        'Returns',
+        'Cult Concierge',
+    ],
+    dropDownMenuItem: [
+        {
+            title: 'About',
+            voices: [
+
+                'About',
+                'Careers',
+                'Glossary',
+                'People and Planet',
+            ],
+            menuOpen: false,
+        },
+        {
+            title: 'Legal',
+            voices: [
+                'Terms & Conditions',
+                'Privacy Policy',
+                'Coockie Policy',
+                'Modern Slavery Policy',
+                'Accessibility',
+            ],
+            menuOpen: false,
+        },
+        {
+            title: 'Find Out More',
+            voices: [
+                'Cult Status',
+                'Promotions',
+                'Affiliates',
+                'Refer A Friend',
+            ],
+            menuOpen: false,
+        },
+    ],
+    cart: []
 });
