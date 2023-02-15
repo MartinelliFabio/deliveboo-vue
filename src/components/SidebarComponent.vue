94e861c1db0c7a631347e1da497de0fecc9023a3
Giovanni Andresini <andresini.giovanni@gmail.com>
2023-02-15 18:34:12 +0100

    filter branch

---
+++ src/components/SidebarComponent.vue
@@ -0,0 +1,99 @@
+<template>
+    <!-- <div class="d-flex flex-column flex-shrink-0 p-3 text-white bg-dark" style="width: 250px; ">
+    <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+      <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
+      <span class="fs-4">Sidebar</span>
+    </a>
+    <hr>
+    <ul class="nav nav-pills flex-column mb-auto">
+      <li class="nav-item">
+        <a href="#" class="nav-link active" aria-current="page">
+          <svg class="bi me-2" width="16" height="16"><use xlink:href="#home"></use></svg>
+          Home
+        </a>
+      </li>
+      <li>
+        <a href="#" class="nav-link text-white">
+          <svg class="bi me-2" width="16" height="16"><use xlink:href="#speedometer2"></use></svg>
+          Dashboard
+        </a>
+      </li>
+      <li>
+        <a href="#" class="nav-link text-white">
+          <svg class="bi me-2" width="16" height="16"><use xlink:href="#table"></use></svg>
+          Orders
+        </a>
+      </li>
+      <li>
+        <a href="#" class="nav-link text-white">
+          <svg class="bi me-2" width="16" height="16"><use xlink:href="#grid"></use></svg>
+          Products
+        </a>
+      </li>
+      <li>
+        <a href="#" class="nav-link text-white">
+          <svg class="bi me-2" width="16" height="16"><use xlink:href="#people-circle"></use></svg>
+          Customers
+        </a>
+      </li>
+    </ul>
+    <hr>
+  
+  </div> -->
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.side-bar {
+  width: 15%;
+  min-width: 250px;
+  min-height: 100vh;
+  background-color: gray;
+  padding: 1rem;
+
+  ul {
+    .title {
+      color: white;
+      text-transform: uppercase;
+      font-size: 1.5rem;
+      font-weight: bold;
+      padding: 0.8rem;
+    }
+
+    li {
+      font-size: 1.1rem;
+      padding: 0.8rem;
+
+      div {
+        display: inline-block;
+      }
+
+      i {
+        margin-right: 10px;
+      }
+    }
+  }
+
+  a {
+    color: white;
+  }
+}
+
+@media screen and (max-width: 992px) and (min-width: 300px) {
+  .side-bar {
+    width: auto;
+    min-width: 80px;
+
+    ul {
+      li {
+        div {
+          display: none;
+        }
+      }
+    }
+  }
+}
+</style>
--- src/pages/ShopkeeperList.vue
+++ src/pages/ShopkeeperList.vue
@@ -1,21 +1,49 @@
 <template>
   <HeroComponent :isVisible="false"></HeroComponent>
-    <div class="d-flex justify-content-center mt-5">
-        <h2>Ristoranti</h2>
-      </div>
+  <div class="d-flex justify-content-center pt-4">
+    <h2 style="font-size: 54px">Ristoranti</h2>
+  </div>
 
-      <!-- <ul class="filters_menu d-flex justify-content-center">
-        <li class="active" data-filter="*">All</li>
-        <li data-filter=".burger">Burger</li>
-        <li data-filter=".pizza">Pizza</li>
-        <li data-filter=".pasta">Pasta</li>
-        <li data-filter=".fries">Fries</li>
-      </ul> -->
+  <!-- <div class="container mt-3 d-flex justify-content-center">
+    <div class="form-check ms-2" v-for="type in types" :key="type.id">
+      <input
+        class="form-check-input"
+        type="checkbox"
+        :value="type.id"
+        :id="type.id"
+        v-model="typeFilter"
+      />
+      <label class="form-check-label text-capitalize" :for="type.id">{{
+        type.name
+      }}</label>
+    </div>
+  </div> -->
+
+  <div class="container mt-3 d-flex justify-content-center">
+    <div class="form-check ms-2" v-for="type in types" :key="type.id">
+      <input
+        class="form-check-input"
+        type="checkbox"
+        name="types"
+        :value="type.id"
+        :id="type.id"
+        v-model="typeFilter"
+      />
+      <label class="form-check-label text-capitalize" :for="type.id">{{
+        type.name
+      }}</label>
+    </div>
+  </div>
+
   <div class="row justify-content-around my-cont container">
-    <div class="col-sm-12 col-lg-6 col-md-12 col-xl-6 col-xxl-4" v-for="(shopkeeper, i) in shopkeepers" :key="i">
-      <router-link :to="`shopkeepers/${shopkeeper.slug} `" >
-      <ShopkeeperCardComponent :shopkeeper="shopkeeper" />
-     </router-link>
+    <div
+      class="col-sm-12 col-lg-6 col-md-12 col-xl-6 col-xxl-4"
+      v-for="shopkeeper in filteredShopkeepers"
+      :key="shopkeeper.id"
+    >
+      <router-link :to="`shopkeepers/${shopkeeper.slug} `">
+        <ShopkeeperCardComponent :shopkeeper="shopkeeper" />
+      </router-link>
     </div>
   </div>
 </template>
@@ -25,71 +53,115 @@ import axios from "axios";
 import HeroComponent from "../components/HeroComponent.vue";
 import ProductCardComponent from "../components/ProductCardComponent.vue";
 import ShopkeeperCardComponent from "../components/ShopkeeperCardComponent.vue";
+import SidebarComponent from "../components/SidebarComponent.vue";
 import { store } from "../store.js";
 export default {
   name: "ShopkeeperList",
-  components: { ProductCardComponent, HeroComponent,ShopkeeperCardComponent },
+  components: {
+    ProductCardComponent,
+    HeroComponent,
+    ShopkeeperCardComponent,
+    SidebarComponent,
+  },
   data() {
     return {
       store,
       products: [],
-      shopkeepers:[]
-
+      shopkeepers: [],
+      types: [],
+      typeFilter: [],
     };
   },
   methods: {
     getProducts() {
       axios.get(`${this.store.apiUrl}/products`).then((response) => {
-        console.log(response.data.results);
+        // console.log(response.data.results);
         this.products = response.data.results;
       });
     },
+
     getShopkeepers() {
       axios.get(`${this.store.apiUrl}/shopkeepers`).then((response) => {
-        console.log(response.data.results);
+        // console.log(response.data.results);
         this.shopkeepers = response.data.results;
       });
-    }
+    },
+
+    getTypes() {
+      axios.get(`${this.store.apiUrl}/types`).then((response) => {
+        // console.log(response.data.results);
+        this.types = response.data.results;
+      });
+    },
+
+    filterShopkeepers() {
+      if (this.typeFilter.length === 0) {
+        return this.shopkeepers;
+      } else {
+        const filteredShopkeepers = this.shopkeepers.filter((shopkeeper) => {
+          let check = false;
+          this.typeFilter.forEach((element) => {
+            if (shopkeeper.types.includes(element)) {
+              check = true;
+            }
+          });
+          return check;
+        });
+        // const filteredShopkeepers = this.typeFilter.includes(this.shopkeepers.type_id);
+        console.log(filteredShopkeepers);
+        return filteredShopkeepers;
+      }
+    },
   },
   mounted() {
     this.getProducts();
     this.getShopkeepers();
+    this.getTypes();
+  },
+
+  computed: {
+    filteredShopkeepers() {
+      return this.filterShopkeepers();
+    },
   },
 };
 </script>
 
 <style lang="scss" scoped>
-
 $white: #ffffff;
 $black: #000000;
 $primary1: #ffbe33;
 $primary2: #222831;
 $textCol: #1f1f1f;
-.my-cont{
-    // width: 50%;
-    margin: auto;
-    margin-top: 4rem;
+.my-cont {
+  // width: 50%;
+  margin: auto;
+  margin-top: 2rem;
 }
 
 .filters_menu {
-    padding: 0;
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
-    list-style-type: none;
-    margin: 45px 0 20px 0;
+  padding: 0;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  list-style-type: none;
+  margin: 45px 0 20px 0;
 
-    li {
-      padding: 7px 25px;
-      cursor: pointer;
-      border-radius: 25px;
+  li {
+    padding: 7px 25px;
+    cursor: pointer;
+    border-radius: 25px;
 
-      &.active {
-        background-color: $primary2;
-        color: $white;
-      }
+    &.active {
+      background-color: $primary2;
+      color: $white;
     }
   }
+}
 
-
+.my-position {
+  position: absolute;
+  left: 0;
+  top: 0;
+}
 </style>
